c15b7658cc66505b0acd163dcec3a6903d83ff06
Pierre EVEN <spam@evenpierre.fr>
2025-06-03 18:24:30 +0200

    Added github ci files

---
+++ scripts/download_run.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+wget -O /opt/schedulator/schedulator.zip https://github.com/Unreal-Engine-FR/schedulator/releases/latest/download/schedulator_linux.zip
+unzip /opt/schedulator/schedulator.zip -d /opt/schedulator
+rm /opt/schedulator/schedulator.zip
+mv /opt/schedulator/schedulator/schedulator /opt/schedulator/schedulator_exe
+rmdir /opt/schedulator/schedulator/
+chmod u+x /opt/schedulator/schedulator_exe
+/opt/schedulator/schedulator_exe /saved/config.json
