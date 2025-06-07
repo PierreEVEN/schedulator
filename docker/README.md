309b86f3ca42618cf7db807ae101b71cf0e5bd33
Pierre EVEN <spam@evenpierre.fr>
2025-06-07 16:40:11 +0200

    improved docker files

--- docker/README.md
+++ docker/README.md
@@ -1,6 +1,3 @@
 # Docker
 
-## Setup
-
-Edit `./compose.yaml` and set schedulator's volume for certificates
-Edit `./config.json` and set certificates file paths
+Don't forget to set path to certificates in [.env](./.env)
--- docker/scripts/download_run.sh
+++ docker/scripts/download_run.sh
@@ -4,15 +4,13 @@ VERSION=${1:-latest}
 
 # Download and extract desired schedulator version
 wget -O /opt/schedulator/schedulator.zip https://github.com/PierreEVEN/schedulator/releases/$VERSION/download/schedulator_linux.zip
-unzip /opt/schedulator/schedulator.zip -d /opt/schedulator
+unzip /opt/schedulator/schedulator.zip -d /opt
 
 # Install
-mv /opt/schedulator/schedulator/schedulator /opt/schedulator/schedulator_exe
-chmod u+x /opt/schedulator/schedulator_exe
+chmod u+x /opt/schedulator/schedulator
 
 # Cleanup
 rm /opt/schedulator/schedulator.zip
-rm -r /opt/schedulator/schedulator/
 
 # Run
-/opt/schedulator/schedulator_exe
+/opt/schedulator/schedulator
